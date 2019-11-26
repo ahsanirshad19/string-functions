db88a45f0bea13c3489d5fd80a578210d05a0afa
ahsanirshad19 <58226201+ahsanirshad19@users.noreply.github.com>
2019-11-26 21:18:02 +0500

    String functions
    All string function and an equation solved with PEMDAS rule

---
+++ Assignment (all in one).py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# # 1 all strings used
+
+# In[15]:
+
+
+#captalize
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.capitalize())
+
+
+# In[25]:
+
+
+#center
+print("my name is ahsan")
+name="my  name is ahsan"
+print(name.center(100))
+
+
+# In[30]:
+
+
+#count 
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.count("a"))
+
+
+# In[67]:
+
+
+#endwith
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.zfill(100))
+
+
+# In[68]:
+
+
+#upper
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.upper())
+
+
+# In[85]:
+
+
+#Translate
+print("my name is ahsan")
+name="my name is ahsan"
+name="main kya krn"
+print(name.translate(name))
+
+
+# In[86]:
+
+
+#Title
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.title())
+
+
+# In[87]:
+
+
+#swapcase
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.swapcase())
+
+
+# In[88]:
+
+
+#strip
+print("            my name is ahsan   ")
+name="             my name is ahsan   "
+print(name.strip())
+
+
+# In[97]:
+
+
+#startwith
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.startswith(my))
+print(name.startswith(name))
+
+
+# In[98]:
+
+
+#splitlines
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.splitlines())
+
+
+# In[99]:
+
+
+#split
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.split())
+
+
+# In[102]:
+
+
+#rpartition
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.rpartition(name))
+
+
+# In[108]:
+
+
+#rjust
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.rjust(50))
+
+
+# In[116]:
+
+
+#rindex
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.rindex(name))
+
+
+# In[125]:
+
+
+#rfind
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.rfind(name))
+
+
+# In[136]:
+
+
+#replace
+print("my name is ahsan")
+name="my name is ahsan"
+notname="not ahsan"
+print(notname.replace(name,notname))
+
+
+# In[139]:
+
+
+#lower
+print("MYy name is ahsan")
+name="MYy name is ahsan"
+print(name.lower())
+
+
+# In[149]:
+
+
+#join
+print("my name is ahsan")
+name="my name is ahsan"
+notname="not ahsan"
+print(notname.join(name))
+
+
+# In[153]:
+
+
+#isupper
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.isupper())
+
+
+# In[154]:
+
+
+#istitle
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.istitle())
+
+
+# In[155]:
+
+
+#isspace
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.isspace())
+
+
+# In[156]:
+
+
+#islower
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.islower())
+
+
+# In[157]:
+
+
+#isdigit
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.isdigit())
+
+
+# In[158]:
+
+
+#isalpha
+print("my name is ahsan")
+name="my name is ahsan"
+print(name.isalpha())
+
+
+# In[169]:
+
+
+#format
+print("MMMy name is ahsan")
+name="MMMy name is ahsan"
+print(name.format(100))
+
+
+# In[186]:
+
+
+#find
+print("my name is ahsan")
+name="my name is ahsan"
+ahsan="ahsan"
+print(name.find(ahsan))
+
+
+# # 2. aplly all operation on some operands
+# 
+
+# In[200]:
+
+
+print(8+5-10*22/5 %10)
+
+
+# In[ ]:
+
+
+
+
